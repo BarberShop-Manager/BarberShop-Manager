503ef2c4a5376a4bdc512f4176b37e8391dc1940
mateus-de-siqueira <mateusiqueira.s@gmail.com>
2021-03-20 03:07:45 -0300

    Adicionando a primeira parte do css nas páginas do cliente :hammer:

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,25 @@
+var menu = "0"
+
+//Função que oberseva o tamanho da tela
+
+function myFunction(x) {
+    if (x.matches) { // Se a tela for desktop
+        menu = "25%"
+    } else { //Se a tela for mobile
+        menu = "50%"
+    }
+}
+
 function abrir_menu() {
-    document.getElementById("menu").style.width = "50%"
+    document.getElementById("menu").style.width = menu
+    document.getElementById("esconder").style.width = "100%"
 }
 
 function fechar_menu() {
     document.getElementById("menu").style.width = "0"
+    document.getElementById("esconder").style.width = "0"
 }
+
+var x = window.matchMedia("(min-width: 1366px)")
+myFunction(x) // Chama a função que observa em tempo de execução
+x.addListener(myFunction) 
