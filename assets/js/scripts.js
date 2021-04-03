0ae2991f01534fa023af1676e589357cc4c57c02
mateus-de-siqueira <mateusiqueira.s@gmail.com>
2021-04-03 18:38:02 -0300

    corrgindo a pagina de login

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,12 +20,6 @@ function fechar_menu() {
     document.getElementById("esconder").style.width = "0"
 }
 
-<<<<<<< HEAD
-var x = window.matchMedia("(min-width: 1366px)")
-myFunction(x) // Chama a função que observa em tempo de execução
-x.addListener(myFunction) 
-=======
 var x = window.matchMedia("(min-width: 992px)")
 myFunction(x) // Chama a função que observa em tempo de execução
 x.addListener(myFunction)
->>>>>>> frontend-cliente
