844bcdb7f3afcfb0975df19678dc3c3b87c24417
Peedrooo <pedrovitora.jesus@gmail.com>
2021-04-03 11:01:40 -0300

    Manutenção modal e linkagem

---
+++ assets/js/modal_cadastro.js
@@ -0,0 +1,39 @@
+
+function iniciaBadModal(bad) {
+    const modal0 = document.getElementById('modal_confirmacao_falha');
+    modal0.classList.add('falha'); 
+    }
+    const botao0 =document .querySelector('#botao-agend-cancel');
+    botao0.addEventListener('click',function(){
+        iniciaBadModal('modal_confirmacao_falha')
+    });
+
+
+
+function iniciaGoodModal(good) {
+    const modal1 = document.getElementById('modal_confirmacao_sucesso');
+    modal1.classList.add('sucesso'); 
+    }
+
+    const botao1 = document .querySelector('#botao-agend-confirm');
+    botao1.addEventListener('click', function(){
+        iniciaGoodModal('modal_confimacao_sucesso')
+    });
+
+
+function iniciaTrabalhoModal(work){
+    const modal2 = document .getElementsById('modal_dia_trabalho');
+    modal2.classList.add('ok');
+    
+    }
+
+    iniciaTrabalhoModal('trabalho');
+
+
+    /*
+    const botao2 = document .querySelector('.box-container');
+    botao2.addEventListener('click', function(){
+        iniciaTrabalhoModal('trabalho')
+    });
+    */
+
