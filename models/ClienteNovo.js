173bcb0bdba2b07a06089c515bac5f2c00586bad
Gabriel <gabrielfsacademico@gmail.com>
2021-04-26 16:53:39 -0300

    Pensando para o mongodb :recycle:

--- models/ClienteNovo.js
+++ models/ClienteNovo.js
@@ -1,37 +1,4 @@
-const Sequelize  = require('sequelize');
-const sequelize = new Sequelize('db_bsm', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const db = require("./db");
+const {Op} = require("sequelize");
 
-const userNew = sequelize.define('users',{
-    user_id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true
-    },
-    _name: {
-        type: Sequelize.STRING(30)
-    },
-    _username: {
-        type: Sequelize.STRING(30)
-    },
-    _email: {
-        type: Sequelize.STRING(60)
-    },
-    _cpf: {
-        type: Sequelize.INTEGER
-    },
-    _date: {
-        type: Sequelize.DATE
-    },
-    _password: {
-        type: Sequelize.INTEGER(100)
-    },
-    _telephone: {
-        type: Sequelize.INTEGER
-    },
-    _nivel: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0
-    }
-}); 
+
