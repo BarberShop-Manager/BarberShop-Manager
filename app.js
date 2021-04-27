155d9f38fd990dd8898b9f8606c237e7de75e027
Mateus-de-Siqueira <mateusiqueira.s@gmail.com>
2021-04-27 19:49:46 -0300

    Fazendo correçoes parte 1

--- app.js
+++ app.js
@@ -8,55 +8,55 @@ const admin = require("./routes/admin");
 const employee = require("./routes/employee");
 const cliente = require("./routes/client");
 const path = require("path");
-const Sequelize  = require('sequelize');
+const Sequelize = require('sequelize');
 // CONFIGURAÇÕES
-    //Body Parser
-        app.use(bodyParser.urlencoded({ extended: true }));
-        app.use(bodyParser.json());
-    //Handlebars
-        app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
-        app.set('view engine', 'handlebars');
-    //Sequelize
-        const sequelize = new Sequelize('db_bsm', 'root', '', {
-            host: 'localhost',
-            dialect: 'mysql'
-        });
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//Handlebars
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+//Sequelize
+const sequelize = new Sequelize('db_bsm', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
 
 
-    //Public
-        app.use(express.static(path.join(__dirname, "public")))
+//Public
+app.use(express.static(path.join(__dirname, "public")))
 //ROTAS
 
-    //Rota Principal
-        app.get('/', (req, res) => {
-            res.render("index")
-        });
+//Rota Principal
+app.get('/', (req, res) => {
+    res.render("index")
+});
 
-    //Cadatro do Cliente
-        app.get('/cadastro', (req, res) => {
-            res.render("cadastro-cliente")
-        });
+//Cadatro do Cliente
+app.get('/cadastro', (req, res) => {
+    res.render("cadastro-cliente")
+});
 
-        app.post('/cadastro/novo',(req,res)=>{
-            res.render("novo")
-        })
+app.post('/cadastro/novo', (req, res) => {
+    res.render("novo")
+})
 
-    //Login dos Usuários
-        app.get('/login', (req, res) => {
-            res.render("login")
-        });
+//Login dos Usuários
+app.get('/login', (req, res) => {
+    res.render("login")
+});
 
-    //Rotas do Adminstrador
-        app.use('/administrador', admin);
+//Rotas do Adminstrador
+app.use('/administrador', admin);
 
-    //Rotas do Funcionários
-        app.use('/funcionario', employee);
+//Rotas do Funcionários
+app.use('/funcionario', employee);
 
-    //Rotas do Cliente
-        app.use('/cliente', cliente);
+//Rotas do Cliente
+app.use('/cliente', cliente);
 
 //OUTRAS CONFIGURAÇÕES
-    const PORT = 3308;
-    app.listen(PORT, () => {
-        console.log("Barbearia Aberta !")
-    })
+const PORT = 3308;
+app.listen(PORT, () => {
+    console.log(`Barbearia rodando no seguinte servidor: http://localhost:${PORT}`)
+})
