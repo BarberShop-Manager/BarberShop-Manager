872fdb204d4a9461d07661224ff35fe805b1de34
Peedrooo <pedrovitora.jesus@gmail.com>
2021-04-27 15:39:17 -0300

    Conectando com a base de dados

---
+++ models/ServicoNovo.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose")
+const { stringify } = require("node:querystring");
+
+
+// Configurando o mongoose
+    mongoose.Promisse = global.Promise;
+    mongoose.connect("mongodb://localhost/banco",{
+        useMongoClient: true
+    }).then(function(){
+        console.log("MongoDb Conectado")
+    }).catch(function(err){
+        console.log("Erro ao se conectar a base de dados: "+err)
+    })
+
+// Model - cadastro
+    const servicocad = mongoose.Schema({
+        nome_do_funcionario:{
+            type:String,
+            require: true,
+        },
+        Selecionar_servico:{
+            type: String,
+            require: true,
+        },
+        data_do_servico:{
+            type:Date,
+            require: true,
+            
+        },
+        horario:{
+            type:Date,
+            require: true,
+        },
+        pagamento:{
+            type:String,
+            require: true,
+        },
+        numero:{
+            type:Number,
+            require: true,
+        },
+        observações:{
+            type: String
+        }
+    })
+
+mongoose.model('cad-horario',servicocad)
